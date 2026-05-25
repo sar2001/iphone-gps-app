4668d59c7bf88949702c3bc45388b87a00f2e83b
sar2001 <r.schwaderer@t-online.de>
2026-05-25 10:30:30 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "gps-app-v1";
+
+const FILES = [
+
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+
+  e.waitUntil(
+
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+
+  e.respondWith(
+
+    caches.match(e.request)
+    .then(response => response || fetch(e.request))
+  );
+});
